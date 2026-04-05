b66a22480f3719bd32f1509c3cad94cc7c7a7ccb
doayt <1772520975@qq.com>
2026-04-05 15:57:23 +0800

    工程框架

---
+++ chat-api-interfaces/src/main/java/chat/interfaces/ApiApplication.java
@@ -0,0 +1,18 @@
+package chat.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
